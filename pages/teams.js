7d140b7fde35e2d962270a62aad7fc7837eaea4d
Parmeet <sparmeet204@gmail.com>
2022-08-19 20:31:25 +0530

    Updated UI.

--- pages/teams.js
+++ pages/teams.js
@@ -10,7 +10,8 @@ const Teams = () => {
         prevArrow: <button type="button" className="slick-prev"><svg className="icon icon-arrow-prev"><use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="img/sprite.svg#icon-arrow-prev"></use></svg></button>,
         nextArrow: <button type="button" className="slick-next"><svg className="icon icon-arrow-next"><use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="img/sprite.svg#icon-arrow-next"></use></svg></button>,
         speed: 700,
-        autoplay: true,
+        // autoplay: true,
+        rows: 2,
         responsive: [{
             breakpoint: 1200,
             settings: {
@@ -39,73 +40,113 @@ const Teams = () => {
                                 <Slider className="teams__slider js-teams-slider" {...settings}>
                                     <div className="teams__slide" data-aos="animation-scale-y" data-aos-delay="200">
                                         <div className="teams__line" style={{backgroundColor: "#5956E9"}}></div>
-                                        <div className="teams__name" style={{color: "#C75C6F"}}>UI8</div>
+                                        <div className="teams__name" style={{color: "#C75C6F"}}>Website</div>
                                         <div className="teams__text">
-                                            “ Collab is fast. Files are always up to date.
-                                            <br/>
-                                            It’s easy to share designs across the organization, so collaboration is easy ”
+                                            “ Team did a great job for our front end tasks. They are knowledgeable and worked fast and efficiently in completing the tasks. ”
                                         </div>
                                         <div className="teams__user">
                                             <div className="teams__ava">
                                                 <img className="teams__pic" src="img/ui8.png" alt="" />
                                             </div>
                                             <div className="teams__box">
-                                                <div className="teams__man">Dash</div>
-                                                <div className="teams__login">Founder @UI8</div>
+                                                <div className="teams__man">Fran</div>
+                                                <div className="teams__login">Founder Website</div>
                                             </div>
                                         </div>
                                     </div>
                                     <div className="teams__slide" data-aos="animation-scale-y" data-aos-delay="400">
                                         <div className="teams__line" style={{backgroundColor: "#FAB8C4"}}></div>
-                                        <div className="teams__name" style={{color: "#5956E9"}}>Bento</div>
+                                        <div className="teams__name" style={{color: "#5956E9"}}>Website</div>
                                         <div className="teams__text">
-                                            “ Collab is fast. Files are always up to date.
-                                            <br/>
-                                            It’s easy to share designs across the organization, so collaboration is easy ”
+                                            “ Completed the work as per schedule and demonstrated the approaches while consultation. ”
                                         </div>
                                         <div className="teams__user">
                                             <div className="teams__ava">
                                                 <img className="teams__pic" src="img/ava-1.png" alt="" />
                                             </div>
                                             <div className="teams__box">
-                                                <div className="teams__man">Emily</div>
-                                                <div className="teams__login">Founder @Bento</div>
+                                                <div className="teams__man">Kiren Sreekumar</div>
+                                                <div className="teams__login">Founder Website</div>
                                             </div>
                                         </div>
                                     </div>
                                     <div className="teams__slide">
                                         <div className="teams__line" style={{backgroundColor: "#5956E9"}}></div>
-                                        <div className="teams__name" style={{color: "#C75C6F"}}>UI8</div>
+                                        <div className="teams__name" style={{color: "#C75C6F"}}>Website</div>
                                         <div className="teams__text">
-                                            “ Collab is fast. Files are always up to date.
-                                            <br/>
-                                            It’s easy to share designs across the organization, so collaboration is easy ”
+                                            “ Solid work. Can't go wrong. ”
                                         </div>
                                         <div className="teams__user">
                                             <div className="teams__ava">
                                                 <img className="teams__pic" src="img/ui8.png" alt="" />
                                             </div>
                                             <div className="teams__box">
-                                                <div className="teams__man">Dash</div>
-                                                <div className="teams__login">Founder @UI8</div>
+                                                <div className="teams__man">Sean Adler</div>
+                                                <div className="teams__login">Founder Website</div>
                                             </div>
                                         </div>
                                     </div>
                                     <div className="teams__slide">
                                         <div className="teams__line" style={{backgroundColor: "#FAB8C4"}}></div>
-                                        <div className="teams__name" style={{color: "#5956E9"}}>Bento</div>
+                                        <div className="teams__name" style={{color: "#5956E9"}}>Website</div>
                                         <div className="teams__text">
-                                            “ Collab is fast. Files are always up to date.
-                                            <br/>
-                                            It’s easy to share designs across the organization, so collaboration is easy ”
+                                            “ There appears to be no topics that this team cannot quickly understand and become proficient and provide desired result in a very short time frame. They consider many ways to resolve our issues, considering multiple aspects as well (ie. Performance, accuracy, simplicity). Their feedback is documented and easy to understand. They thoroughly tests all of their work which reduces the amount of time spent by ourselves during acceptance phases. They quickly grasps business processes and business issues. They provide a close follow-up by directly contacting us which shows that they are dedicated to complete tasks. I look forward to work with em on challenging subjects which, for sure, will become successes again. ”
                                         </div>
                                         <div className="teams__user">
                                             <div className="teams__ava">
                                                 <img className="teams__pic" src="img/ava-1.png" alt="" />
                                             </div>
                                             <div className="teams__box">
-                                                <div className="teams__man">Emily</div>
-                                                <div className="teams__login">Founder @Bento</div>
+                                                <div className="teams__man">Samba Thibo</div>
+                                                <div className="teams__login">Founder Website</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="teams__slide">
+                                        <div className="teams__line" style={{backgroundColor: "#FAB8C4"}}></div>
+                                        <div className="teams__name" style={{color: "#5956E9"}}>Website</div>
+                                        <div className="teams__text">
+                                            “ Absolute 5 star experience. These guys are pros - excellent communication, prompt and really competent professionals! ”
+                                        </div>
+                                        <div className="teams__user">
+                                            <div className="teams__ava">
+                                                <img className="teams__pic" src="img/ava-1.png" alt="" />
+                                            </div>
+                                            <div className="teams__box">
+                                                <div className="teams__man">Jack Fischman</div>
+                                                <div className="teams__login">Founder Website</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="teams__slide">
+                                        <div className="teams__line" style={{backgroundColor: "#FAB8C4"}}></div>
+                                        <div className="teams__name" style={{color: "#5956E9"}}>Website</div>
+                                        <div className="teams__text">
+                                            “ A complete professional team. Ask and you will receive. Excellent communicators. A pleasure to work with. ”
+                                        </div>
+                                        <div className="teams__user">
+                                            <div className="teams__ava">
+                                                <img className="teams__pic" src="img/ava-1.png" alt="" />
+                                            </div>
+                                            <div className="teams__box">
+                                                <div className="teams__man">Johnny Palomares</div>
+                                                <div className="teams__login">Founder Website</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="teams__slide">
+                                        <div className="teams__line" style={{backgroundColor: "#FAB8C4"}}></div>
+                                        <div className="teams__name" style={{color: "#5956E9"}}>Website</div>
+                                        <div className="teams__text">
+                                            “ Excellent work as usual, very friendly and very dedicated in achieving the best result. ”
+                                        </div>
+                                        <div className="teams__user">
+                                            <div className="teams__ava">
+                                                <img className="teams__pic" src="img/ava-1.png" alt="" />
+                                            </div>
+                                            <div className="teams__box">
+                                                <div className="teams__man">J De Leon</div>
+                                                <div className="teams__login">Founder Website</div>
                                             </div>
                                         </div>
                                     </div>
