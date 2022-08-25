82bef0ca664fe971d4e53d395c961a72b9bb7ee6
Parmeet <sparmeet204@gmail.com>
2022-08-25 20:35:32 +0530

    Updated UI.

--- pages/teams.js
+++ pages/teams.js
@@ -59,7 +59,7 @@ const Teams = () => {
         dots: false,
         arrows: false,
         speed: 700,
-        autoplay: true,
+        autoplay: true
     };
 
     return (
@@ -71,7 +71,8 @@ const Teams = () => {
                         <h2 className="teams__title title">
                             We’re Top Ranked
                             Service Provider
-                            On <Typewriter
+                            On
+                            <Typewriter
                                 options={{
                                     strings: ['Upwork', 'Clutch.co', 'People per Hour'],
                                     autoStart: true,
@@ -86,10 +87,26 @@ const Teams = () => {
                         </div>
                         <div className="teams__col">
                             <div className="teams__container">
-                                <div className='row'>
+                                <div className='row mb-5'>
                                     <div className='col-lg-6' >
                                         <Slider className="teams__slider js-teams-slider" {...settingsRight} >
-                                            <div className="teams__slide" data-aos="animation-scale-y" data-aos-delay="200">
+                                            <div className='row teams-cmnt-row' >
+                                                <div className='col-lg-12 mt-5 teams-cmnt-text'>
+                                                    Solid work. Can't go wrong.
+                                                </div>
+                                                <div className='col-lg-12 mb-5 teams-cmnt-author'>
+                                                    - Sean Adler
+                                                </div>
+                                            </div>
+                                            <div className='row teams-cmnt-row' >
+                                                <div className='col-lg-12 mt-5 teams-cmnt-text'>
+                                                    Completed the work as per schedule and demonstrated the approaches while consultation.
+                                                </div>
+                                                <div className='col-lg-12 mb-5 teams-cmnt-author'>
+                                                    - Kiren Sreekumar
+                                                </div>
+                                            </div>
+                                            {/* <div className="teams__slide" data-aos="animation-scale-y" data-aos-delay="200">
                                                 <div className="teams__line" style={{backgroundColor: "#FAB8C4"}}></div>
                                                 <div className="teams__text">
                                                     Solid work. Can't go wrong.
@@ -110,12 +127,35 @@ const Teams = () => {
                                                         <div className="teams__man">- Kiren Sreekumar</div>
                                                     </div>
                                                 </div>
-                                            </div>
+                                            </div> */}
                                         </Slider>
                                     </div>
                                     <div className='col-lg-6' >
                                         <Slider className="teams__slider js-teams-slider" {...settingsUp} >
-                                            <div className="teams__slide">
+                                            <div className='row teams-cmnt-row' >
+                                                <div className='col-lg-12 mt-5 teams-cmnt-text'>
+                                                    Team did a great job for our front end tasks. They are knowledgeable and worked fast and efficiently in completing the tasks.
+                                                </div>
+                                                <div className='col-lg-12 mb-5 teams-cmnt-author'>
+                                                    - Fran
+                                                </div>
+                                            </div>
+                                            <div className='row teams-cmnt-row' >
+                                                <div className='col-lg-12 mt-5 teams-cmnt-text'>
+                                                    There appears to be no topics that this team cannot quickly understand and become proficient and provide desired result in a very short time frame.
+                                                    They consider many ways to resolve our issues, considering multiple aspects as well (ie. Performance, accuracy, simplicity).
+                                                    Their feedback is documented and easy to understand.
+                                                    They thoroughly tests all of their work which reduces the amount of time spent by ourselves during acceptance phases.
+                                                    They quickly grasps business processes and business issues.
+                                                    They provide a close follow-up by directly contacting us which shows that they are dedicated to complete tasks.
+                                                    I look forward to work with em on challenging subjects which, for sure, will become successes again.
+                                                </div>
+                                                <div className='col-lg-12 mb-5 teams-cmnt-author'>
+                                                    - Samba Thibo
+                                                </div>
+                                            </div>
+
+                                            {/* <div className="teams__slide">
                                                 <div className="teams__line" style={{backgroundColor: "#5956E9"}}></div>
                                                 <div className="teams__text">
                                                     Team did a great job for our front end tasks. They are knowledgeable and worked fast and efficiently in completing the tasks.
@@ -142,12 +182,31 @@ const Teams = () => {
                                                         <div className="teams__man">- Samba Thibo</div>
                                                     </div>
                                                 </div>
-                                            </div>
+                                            </div> */}
                                         </Slider>
                                     </div>
+                                </div>
+                                <div className='row'>
                                     <div className='col-lg-6' >
                                         <Slider className="teams__slider js-teams-slider" {...settingsDown} >
-                                            <div className="teams__slide">
+                                            <div className='row teams-cmnt-row' >
+                                                <div className='col-lg-12 mt-5 teams-cmnt-text'>
+                                                    Absolute 5 star experience. These guys are pros - excellent communication, prompt and really competent professionals!
+                                                </div>
+                                                <div className='col-lg-12 mb-5 teams-cmnt-author'>
+                                                    - Jack Fischman
+                                                </div>
+                                            </div>
+                                            <div className='row teams-cmnt-row' >
+                                                <div className='col-lg-12 mt-5 teams-cmnt-text'>
+                                                    A complete professional team. Ask and you will receive. Excellent communicators. A pleasure to work with.
+                                                </div>
+                                                <div className='col-lg-12 mb-5 teams-cmnt-author'>
+                                                    - Johnny Palomares
+                                                </div>
+                                            </div>
+
+                                            {/* <div className="teams__slide">
                                                 <div className="teams__line" style={{backgroundColor: "#5956E9"}}></div>
                                                 <div className="teams__text">
                                                     Absolute 5 star experience. These guys are pros - excellent communication, prompt and really competent professionals!
@@ -168,12 +227,29 @@ const Teams = () => {
                                                         <div className="teams__man">- Johnny Palomares</div>
                                                     </div>
                                                 </div>
-                                            </div>
+                                            </div> */}
                                         </Slider>
                                     </div>
                                     <div className='col-lg-6' >
                                         <Slider className="teams__slider js-teams-slider" {...settingsLeft} >
-                                            <div className="teams__slide">
+                                            <div className='row teams-cmnt-row' >
+                                                <div className='col-lg-12 mt-5 teams-cmnt-text'>
+                                                    Excellent work as usual, very friendly and very dedicated in achieving the best result.
+                                                </div>
+                                                <div className='col-lg-12 mb-5 teams-cmnt-author'>
+                                                    - J De Leon
+                                                </div>
+                                            </div>
+                                            <div className='row teams-cmnt-row' >
+                                                <div className='col-lg-12 mt-5 teams-cmnt-text'>
+                                                    Available on time, delivery on schedule and excellent communication, this is what one will look while working with a professional, team was on top of all these.
+                                                </div>
+                                                <div className='col-lg-12 mb-5 teams-cmnt-author'>
+                                                    - P. Mishra
+                                                </div>
+                                            </div>
+
+                                            {/* <div className="teams__slide">
                                                 <div className="teams__line" style={{backgroundColor: "#FAB8C4"}}></div>
                                                 <div className="teams__text">
                                                     Excellent work as usual, very friendly and very dedicated in achieving the best result.
@@ -194,7 +270,7 @@ const Teams = () => {
                                                         <div className="teams__man">- P. Mishra</div>
                                                     </div>
                                                 </div>
-                                            </div>
+                                            </div> */}
                                         </Slider>
                                     </div>
                                 </div>
