6acac36efb163d58cb8f0b5f719d90d0a322f53c
Parmeet <sparmeet204@gmail.com>
2022-09-16 21:22:41 +0530

    Updated UI for Footer.

--- pages/includes/footer.js
+++ pages/includes/footer.js
@@ -38,73 +38,114 @@ const Footer = () => {
         setSocialIcons(socialIconsStatus);
     }
     return (
-        <div className="footer js-footer">
+        <div className="footer" style={{ overflow: "hidden" }}>
             <div className="footer__center center">
-                <div className="footer__row">
-                    {/* <div className="footer__col">
-                        <a className="footer__logo" href="#">
-                            <img className="footer__pic" src="logo/logoEdited.png" alt="" />
-                        </a>
-                    </div> */}
-                    <div onClick={() => showAboutUs()} className={ aboutUs ? "footer__col active" : "footer__col"} style={{paddingTop: "8px"}} >
-                        <div className="footer__category js-footer-category">
-                            About Us
+                <div className="footer__row pt-5">
+                    <div onClick={() => showAboutUs()} className={ aboutUs ? "footer__col col-lg-4 col-md-4 active" : "footer__col col-lg-4 col-md-4"} >
+                        <div className="footer__category footer-category-cgt-name">
+                            Code Garage
                             <svg className="icon icon-arrow-down">
                                 <use xlinkHref="img/sprite.svg#icon-arrow-down"></use>
                             </svg>
                         </div>
                         <div className="footer__menu" style={ aboutUs ? { display: "block" } : { display: "none" }} >
-                            We're one of the first class development and consulting organization situated in Mohali India. We’re in business just like you - we understand how stressful it can be to work on projects with outside vendors, especially when large budgets are involved.
+                            <span className="footer__link footer-link-about" >We’re a design driven product development studio.</span>
+                            <span className="footer__link footer-link-contact" >+91 758 910 3409</span>
+                            <span className="footer__link footer-link-contact" >hr@codegaragetech.com</span>
+                            <span className="footer__link footer-link-about" >
+                                The Atrium Quarkcity,<br/>
+                                industrial area, SAS-Nagar,<br/>
+                                Mohali, Punjab -160059
+                            </span>
                         </div>
                     </div>
-                    <div onClick={() => showTechnologies()} className={ technologies ? "footer__col js-footer-col active" : "footer__col js-footer-col"}>
-                        <div className="footer__category js-footer-category">
+                    <div onClick={() => showTechnologies()} className={ technologies ? "footer__col col-lg-4 col-md-4 active" : "footer__col col-lg-4 col-md-4"}>
+                        <div className="footer__category">
                             Technologies We Work On
                             <svg className="icon icon-arrow-down">
                                 <use xlinkHref="img/sprite.svg#icon-arrow-down"></use>
                             </svg>
                         </div>
                         <div className="footer__menu" style={ technologies ? { display: "block" } : { display: "none" }} >
-                            <span className="footer__link" >Ruby on Rails</span>
-                            <span className="footer__link" >NodeJs</span>
-                            <span className="footer__link" >React Native</span>
-                            <span className="footer__link" >Laravel</span>
-                            <span className="footer__link" >ReactJs / NextJs</span>
-                            <span className="footer__link" >VueJs / NuxtJs</span>
+                            <div className='row' >
+                                <div className='col-lg-6 mb-4' >
+                                    <div className='row' >
+                                        <div className='col-lg-2 col-md-2 col-sm-2 col-xs-2 col-2 d-flex justify-content-center align-items-center' >
+                                            <img className='footer-tech-img' src='icons/ror.png' alt='' />
+                                        </div>
+                                        <div className='col-lg-10 col-md-10 col-sm-10 col-xs-10 col-10' >
+                                            <span className="footer__link footer-link-technology" >Ruby on Rails</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='col-lg-6 mb-4' >
+                                    <div className='row' >
+                                        <div className='col-lg-2 col-md-2 col-sm-2 col-xs-2 col-2 d-flex justify-content-center align-items-center' >
+                                            <img className='footer-tech-img' src='images/node_2.png' alt='' />
+                                        </div>
+                                        <div className='col-lg-10 col-md-10 col-sm-10 col-xs-10 col-10' >
+                                            <span className="footer__link footer-link-technology" >NodeJs</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='col-lg-6 mb-4' >
+                                    <div className='row' >
+                                        <div className='col-lg-2 col-md-2 col-sm-2 col-xs-2 col-2 d-flex justify-content-center align-items-center' >
+                                            <img className='footer-tech-img' src='icons/reactjs.png' alt='' />
+                                        </div>
+                                        <div className='col-lg-10 col-md-10 col-sm-10 col-xs-10 col-10' >
+                                            <span className="footer__link footer-link-technology" >React Native</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='col-lg-6 mb-4' >
+                                    <div className='row' >
+                                        <div className='col-lg-2 col-md-2 col-sm-2 col-xs-2 col-2 d-flex justify-content-center align-items-center' >
+                                            <img className='footer-tech-img' src='icons/laravel.png' alt='' />
+                                        </div>
+                                        <div className='col-lg-10 col-md-10 col-sm-10 col-xs-10 col-10' >
+                                            <span className="footer__link footer-link-technology" >Laravel</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='col-lg-6 mb-4' >
+                                    <div className='row' >
+                                        <div className='col-lg-2 col-md-2 col-sm-2 col-xs-2 col-2 d-flex justify-content-center align-items-center' >
+                                            <img className='footer-tech-img' src='icons/reactjs.png' alt='' />
+                                        </div>
+                                        <div className='col-lg-10 col-md-10 col-sm-10 col-xs-10 col-10' >
+                                            <span className="footer__link footer-link-technology" >ReactJs / NextJs</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='col-lg-6 mb-4' >
+                                    <div className='row' >
+                                        <div className='col-lg-2 col-md-2 col-sm-2 col-xs-2 col-2 d-flex justify-content-center align-items-center' >
+                                            <img className='footer-tech-img' src='icons/vuejs.png' alt='' />
+                                        </div>
+                                        <div className='col-lg-10 col-md-10 col-sm-10 col-xs-10 col-10' >
+                                            <span className="footer__link footer-link-technology" >VueJs / NuxtJs</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div onClick={() => showContactUs()} className={ contactUs ? "footer__col js-footer-col active" : "footer__col js-footer-col" } >
-                        <div className="footer__category js-footer-category">
-                            Contact Us
-                            <svg className="icon icon-arrow-down">
-                                <use xlinkHref="img/sprite.svg#icon-arrow-down"></use>
-                            </svg>
-                        </div>
-                        <div className="footer__menu" style={ contactUs ? { display: "block" } : { display: "none" }} >
-                            <a className="footer__link" href="#">+0172-4783676</a>
-                            <a className="footer__link" href="#">+91-7589103409</a>
-                            <a className="footer__link" href="#">codegaragetech@123</a>
-                            <a className="footer__link" href="#">hr@codegaragetech.com</a>
-                            <a className="footer__link" href="#">
-                                The Atrium at Quarkcity Lower ground floor,Zone-B, Plot no A45, industrial area, phase 8B, SAS-Nagar, Mohali, Punjab -160059
-                            </a>
-                        </div>
-                    </div>
-                    <div onClick={() => showSocialIcons()} className={ socialIcons ? "footer__col js-footer-col active" : "footer__col js-footer-col" } >
-                        <div className="footer__category js-footer-category">
+                    <div onClick={() => showSocialIcons()} className={ socialIcons ? "footer__col col-lg-4 col-md-4 active" : "footer__col col-lg-4 col-md-4" } >
+                        <div className="footer__category">
                             Check Us on Social Network
                             <svg className="icon icon-arrow-down">
                                 <use xlinkHref="img/sprite.svg#icon-arrow-down"></use>
                             </svg>
                         </div>
-                        <div className="footer__menu footer-social-menu justify-content-evenly" style={ socialIcons ? { display: "flex" } : { display: "none" }} >
-                            <a className="footer__link" href="https://www.facebook.com/CodeGarageTech">
+                        <div className="footer__menu footer-social-menu" style={ socialIcons ? { display: "flex" } : { display: "none" }} >
+                            <a className="footer__link me-3" href="https://www.facebook.com/CodeGarageTech">
                                 <img className="icon icon-header-img" src="images/Facebook3d.png" alt="" />
                             </a>
-                            <a className="footer__link" href="https://twitter.com/CodeGarageTech">
+                            <a className="footer__link me-3" href="https://twitter.com/CodeGarageTech">
                                 <img className="icon icon-header-img" src="images/Twitter3d.png" alt="" />
                             </a>
-                            <a className="footer__link" href="https://www.linkedin.com/company/codegaragetech">
+                            <a className="footer__link me-3" href="https://www.linkedin.com/company/codegaragetech">
                                 <img className="icon icon-header-img" src="images/LinkedIn3d.png" alt="" />
                             </a>
                             <a className="footer__link" href="https://www.instagram.com/codegaragetech/">
@@ -113,18 +154,12 @@ const Footer = () => {
                         </div>
                     </div>
                 </div>
-                <div className="footer__bottom">
-                    <a className="footer__company" href="#">
-                        <img className="footer__pic footer-icon" src="/logo/favicon_white.jpg" alt="" />
-                    </a>
-                    <div className="footer__copyright">© 2022, All Rights Reserved.</div>
-                    <a className="footer__scroll js-link-scroll" href="#header">
-                        <div className="footer__icon">
-                            <img className="footer__pic" src="img/scroll.svg" alt="" />
-                        </div>
-                        <div className="footer__text">Back to top</div>
-                    </a>
-                </div>
+            </div>
+            <div className="footer__bottom footer-bottom">
+                <div className="footer__copyright">© Copyright 2022. CGT</div>
+                <span className="footer__scroll" >
+                    <div className="footer__text">All Right Reserved.</div>
+                </span>
             </div>
         </div>
     )
