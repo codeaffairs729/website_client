79cc5537189726000180e130443fd71fbef6e5c2
cgt <cdeveloper2016@gmail.com>
2022-11-14 20:32:59 +0530

    Updated UI for Carousel in about us page.

--- pages/buildingPicsSlider.js
+++ pages/buildingPicsSlider.js
@@ -1,21 +1,22 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Slider from "react-slick";
 import PrevButton from "../components/prevButton";
 import NextButton from "../components/nextButton";
 
 const BuildingPicsSlider = () => {
+    const [imageIndex, setImageIndex] = useState(0);
+
     const settings = {
-        className: "center building_slider variable-width",
         centerMode: true,
         infinite: true,
-        centerPadding: "100px",
-        slidesToShow: 1,
-        speed: 2000,
-        arrows: true,
+        centerPadding: 0,
+        slidesToShow: 3,
+        speed: 300,
         prevArrow: <PrevButton />,
         nextArrow: <NextButton />,
-        autoplay: true,
-        variableWidth: true,
+        autoplay: false,
+        lazyLoad: true,
+        beforeChange: (currentSlide, nextSlide) => setImageIndex(nextSlide),
         responsive: [{
             breakpoint: 1199,
             settings: {
@@ -32,8 +33,7 @@ const BuildingPicsSlider = () => {
                 arrows: false,
                 pauseOnHover: true,
                 variableWidth: false,
-                slidesToShow: 1,
-                centerPadding: "0px"
+                slidesToShow: 1
             }
         }]
     };
@@ -48,23 +48,23 @@ const BuildingPicsSlider = () => {
                     </div>
                     <div className="col-lg-12" >
                         <Slider {...settings}>
-                            <div className="p-5 building-slide" style={{ width: 500 }}>
-                                <img className='w-100' src="aboutUsImages/DSC_0318.JPG" alt='' />
+                            <div className={ imageIndex === 0 ? "building-slide activeSlide" : "building-slide" } >
+                                <img className='' src="aboutUsImages/DSC_0318.JPG" alt='' />
                             </div>
-                            <div className="p-5 building-slide" style={{ width: 500 }} >
-                                <img className='w-100' src="aboutUsImages/DSC_0319.JPG" alt='' />
+                            <div className={ imageIndex === 1 ? "building-slide activeSlide" : "building-slide" } >
+                                <img className='' src="aboutUsImages/DSC_0319.JPG" alt='' />
                             </div>
-                            <div className="p-5 building-slide" style={{ width: 500 }}>
-                                <img className='w-100' src="aboutUsImages/DSC_0320.JPG" alt='' />
+                            <div className={ imageIndex === 2 ? "building-slide activeSlide" : "building-slide" } >
+                                <img className='' src="aboutUsImages/DSC_0320.JPG" alt='' />
                             </div>
-                            <div className="p-5 building-slide" style={{ width: 500 }}>
-                                <img className='w-100' src="aboutUsImages/DSC_0323.JPG" alt='' />
+                            <div className={ imageIndex === 3 ? "building-slide activeSlide" : "building-slide" } >
+                                <img className='' src="aboutUsImages/DSC_0323.JPG" alt='' />
                             </div>
-                            <div className="p-5 building-slide" style={{ width: 500 }}>
-                                <img className='w-100' src="aboutUsImages/DSC_0324.JPG" alt='' />
+                            <div className={ imageIndex === 4 ? "building-slide activeSlide" : "building-slide" } >
+                                <img className='' src="aboutUsImages/DSC_0324.JPG" alt='' />
                             </div>
-                            <div className="p-5 building-slide" style={{ width: 500 }}>
-                                <img className='w-100' src="aboutUsImages/DSC_0325.JPG" alt='' />
+                            <div className={ imageIndex === 5 ? "building-slide activeSlide" : "building-slide" } >
+                                <img className='' src="aboutUsImages/DSC_0325.JPG" alt='' />
                             </div>
                         </Slider>
                     </div>
