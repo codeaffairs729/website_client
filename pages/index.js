6cb1edcd5da7af3719f1745a5304b95bc46df313
mehakpal <mehakpals@gmail.com>
2023-02-10 20:42:13 +0530

    Removed dynamic import

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,16 @@
 // import styles from '../styles/Home.module.css'
 import Main from "./main";
 import Package from "./package";
-// import Work from "./work";
+import Work from "./work";
 import Layouts from "./layouts";
-// import Access from "./access";
-// import Partners from "./partners";
-// import Design from "./design";
+import Access from "./access";
+import Partners from "./partners";
+import Design from "./design";
 import Teams from "./teams";
 import Like from "./like";
 import Footer from "./includes/footer";
 import Image from "next/image";
 import dynamic from "next/dynamic";
-
-const Work = dynamic(() => import("./work"), {});
-const Access = dynamic(() => import("./access"), {});
-const Partners = dynamic(() => import("./partners"), {});
-const Design = dynamic(() => import("./design"), {});
-
-
 
 export default function Home() {
   return (
