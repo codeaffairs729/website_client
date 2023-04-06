54b03e61e0df94a6ea019b64632b3004ee02dbf0
Mehakpal <cdeveloper2016@gmail.com>
2023-04-06 16:07:59 +0530

    Removed unwanted in web solution page

--- pages/services/web-solutions.js
+++ pages/services/web-solutions.js
@@ -384,44 +384,6 @@ const content7 = () => (
         mobileActive={false}
       />
     </div>
-    <div className="container mt-3">
-      <h2>Stacked form</h2>
-      <form>
-        <div className="mb-3 mt-3">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            placeholder="Enter email"
-            name="email"
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            type="password"
-            className="form-control"
-            id="pwd"
-            placeholder="Enter password"
-            name="pswd"
-          />
-        </div>
-        <div className="form-check mb-3">
-          <label className="form-check-label">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              name="remember"
-            />{' '}
-            Remember me
-          </label>
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </div>
   </>
 )
 
