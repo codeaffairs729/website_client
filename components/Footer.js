3e9a311d3d20bfd912decdb82e76bba67dbab3b5
optimum-developer <kaurparambir638@gmail.com>
2023-06-02 12:35:51 +0530

    footer minor fix

--- components/Footer.js
+++ components/Footer.js
@@ -37,7 +37,7 @@ const Footer = () => {
     useEffect(() => {
       setTimeout(() => {
         setIsLoaded(true)
-      }, '500')
+      }, '1000')
 
       function handleResize() {
         setWindowSize({
