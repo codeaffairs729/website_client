5815a3f3f3f8dfb3bfd97f6dc052b45adedb0377
optimum-developer <kaurparambir638@gmail.com>
2023-06-02 16:59:27 +0530

    Fix meta tag

--- pages/includes/header.js
+++ pages/includes/header.js
@@ -104,7 +104,7 @@ const Header = () => {
           <meta httpEquiv="X-UA-Compatible" content="IE=Edge" />
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=0.96, maximum-scale=1.0"
+            content="width=device-width, initial-scale=0.96, maximum-scale=2.0"
           />
           <meta name="format-detection" content="telephone=no" />
           <link
