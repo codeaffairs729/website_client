bb863787a95d6d248d07c89ef035efdd35c2514f
cdeveloper123 <cdeveloper2016@gmail.com>
2023-06-12 18:04:03 +0530

    feat(twitter script): Add twitter script for monitoring

--- pages/_document.js
+++ pages/_document.js
@@ -63,6 +63,17 @@ export default function Document() {
           gtag('config', 'G-GT3WPQDNTQ');
     `}
         </Script>
+        {/* Add the Twitter conversion tracking code here */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);},
+                s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+                a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+                twq('config','of7j3');
+              `,
+          }}
+        />
 
         {/* Add Google Tag Manager script */}
         {/* <Script
