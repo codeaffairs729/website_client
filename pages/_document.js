20d509fe1d2b03948bcbbc2e34b76c0abf352c5a
farzpal <farzpal@dtime.ai>
2023-06-21 12:04:28 +0530

    chore(google tage manager):add google tag manager script in head and body

--- pages/_document.js
+++ pages/_document.js
@@ -78,6 +78,22 @@ export default function Document() {
               `,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-WZGMB5Q');
+              `,
+          }}
+        />
+        {/* <script>(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WZGMB5Q');</script> */}
 
         {/* Add Google Tag Manager script */}
         {/* <Script
@@ -114,6 +130,18 @@ export default function Document() {
         <CareerServicesSchema />
       </Head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WZGMB5Q"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MRD33TS"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> */}
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <Main />
         <NextScript />
       </body>
