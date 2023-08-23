4e547239e94fed127c86230c40f798279c45743c
optimum-developer <kaurparambir638@gmail.com>
2023-08-23 18:55:59 +0530

    refactor:(devop-section) :modified devops section, added image slider on career page

--- components/DevOpSection.jsx
+++ components/DevOpSection.jsx
@@ -1,25 +1,63 @@
 import Image from 'next/image'
 import style from './../styles/devops.module.css'
+import hireService from '../styles/hireService.module.css'
+const techTitle = 'Our DevOps Culture'
 import { gitlab } from '../utils/data'
 
 const DevOpSection = () => {
   return (
-    <div className={style.container}>
-      <div className={style.image_container}>
-        <h1>Our DevOps Culture</h1>
-        <img
-          src="/dev_ops/devops_main_1.png"
-          // height={500}
-          // width={500}
-          // layout="fill"
-          alt="dev-ops-png"
-        />
-        {/* <div className={style.icons_div}>
-          <img src={gitlab} />
-          <p>GitLab</p>
-        </div> */}
+    <>
+      <div className={hireService.maindevcontainer}>
+        <div className={hireService.hirecontainer}>
+          <div
+            className={`${hireService.techservicebody}  ${hireService.adjustservicebody}`}
+          >
+            <div className={hireService.techservicebodyleft}>
+              <h1 className={hireService.hireh2}>{techTitle}</h1>
+              <ul
+                className={`${hireService.listicon} ${hireService.hire_list_items_services} `}
+              >
+                <li className={hireService.marginebottom16px}>
+                  <p className={hireService.marginleft25px}>
+                    Continues Development & Integeration
+                  </p>
+                </li>
+                <li className={hireService.marginebottom16px}>
+                  <p className={hireService.marginleft25px}>
+                    24 x 7 Monitoring
+                  </p>
+                </li>
+                <li className={hireService.marginebottom16px}>
+                  <p className={hireService.marginleft25px}>
+                    Systematic Planing
+                  </p>
+                </li>
+                <li className={hireService.marginebottom16px}>
+                  <p className={hireService.marginleft25px}>Optimum SEO</p>
+                </li>
+                <li className={hireService.marginebottom16px}>
+                  <p className={hireService.marginleft25px}>
+                    Reliable Search Queries
+                  </p>
+                </li>
+              </ul>
+            </div>
+            <div className="">
+              {/* <img src="/icons/service-icon/cloud.webp"  /> */}
+              {/* <div> */}
+              <Image
+                src="/devops/devops.png"
+                alt="devops image"
+                width={500}
+                height={350}
+                priority={true}
+              />
+              {/* </div> */}
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
