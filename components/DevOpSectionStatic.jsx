043cde1bfc8dd815076beeb831661e74b2f373d3
optimum-developer <kaurparambir638@gmail.com>
2023-08-29 18:56:59 +0530

    Edit(home): Edit devops section, Edit industries section,

--- components/DevOpSectionStatic.jsx
+++ components/DevOpSectionStatic.jsx
@@ -8,48 +8,50 @@ const DevOpsStatic = () => {
   return (
     <>
       <div className={hireService.maindevcontainer}>
-        <div className={hireService.hirecontainer}>
-          <div
-            className={`${hireService.techservicebody}  ${hireService.adjustservicebody}`}
-          >
-            <div className={hireService.techservicebodyleft}>
-              <h1 className={hireService.hireh2}>{techTitle}</h1>
-              <ul
-                className={`${hireService.listicon} ${hireService.hire_list_items_services} `}
-              >
-                <li className={hireService.marginebottom16px}>
-                  <p className={hireService.marginleft25px}>
-                    Continues Development & Integeration
-                  </p>
-                </li>
-                <li className={hireService.marginebottom16px}>
-                  <p className={hireService.marginleft25px}>
-                    24 x 7 Monitoring
-                  </p>
-                </li>
-                <li className={hireService.marginebottom16px}>
-                  <p className={hireService.marginleft25px}>
-                    Systematic Planing
-                  </p>
-                </li>
-                <li className={hireService.marginebottom16px}>
-                  <p className={hireService.marginleft25px}>Optimum SEO</p>
-                </li>
-                <li className={hireService.marginebottom16px}>
-                  <p className={hireService.marginleft25px}>
-                    Reliable Search Queries
-                  </p>
-                </li>
-              </ul>
-            </div>
-            <div className={hireService.imageContainer}>
-              <div className={hireService.topImage}>
-                <Image
-                  src="/devops/devops_main_2.webp"
-                  alt="bottom image"
-                  width={1000}
-                  height={607}
-                />
+        <div className={hireService.adjustmaincontainer}>
+          <div className={hireService.hirecontainer}>
+            <div
+              className={`${hireService.techservicebody}  ${hireService.adjustservicebody}`}
+            >
+              <div className={hireService.techservicebodyleft}>
+                <h1 className={hireService.hireh2}>{techTitle}</h1>
+                <ul
+                  className={`${hireService.listicon} ${hireService.hire_list_items_services} `}
+                >
+                  <li className={hireService.marginebottom16px}>
+                    <p className={hireService.marginleft25px}>
+                      Continues Development & Integeration
+                    </p>
+                  </li>
+                  <li className={hireService.marginebottom16px}>
+                    <p className={hireService.marginleft25px}>
+                      24 x 7 Monitoring
+                    </p>
+                  </li>
+                  <li className={hireService.marginebottom16px}>
+                    <p className={hireService.marginleft25px}>
+                      Systematic Planing
+                    </p>
+                  </li>
+                  <li className={hireService.marginebottom16px}>
+                    <p className={hireService.marginleft25px}>Optimum SEO</p>
+                  </li>
+                  <li className={hireService.marginebottom16px}>
+                    <p className={hireService.marginleft25px}>
+                      Reliable Search Queries
+                    </p>
+                  </li>
+                </ul>
+              </div>
+              <div className={hireService.imageContainer}>
+                <div className={hireService.topImage}>
+                  <Image
+                    src="/devops/devops_main_2.webp"
+                    alt="bottom image"
+                    width={1100}
+                    height={610}
+                  />
+                </div>
               </div>
             </div>
           </div>
