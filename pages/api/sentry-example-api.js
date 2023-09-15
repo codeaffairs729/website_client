028e14c051d2a3cdab6fefcd526b7c1d647d73be
optimum-developer <kaurparambir638@gmail.com>
2023-09-15 15:26:58 +0530

    fix: fix optimization
    -commit changes of 22-aug

--- pages/api/sentry-example-api.js
+++ pages/api/sentry-example-api.js
@@ -1,4 +1,4 @@
-export default function handler(_req, res) {
-  throw new Error('Sentry Example API Route Error')
-  res.status(200).json({ name: 'John Doe' })
-}
+// export default function handler(_req, res) {
+//   throw new Error('Sentry Example API Route Error')
+//   res.status(200).json({ name: 'John Doe' })
+// }
