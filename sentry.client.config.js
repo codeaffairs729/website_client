b52f08ce6e5dde8cb771bd6849be5f9fb7427b29
optimum-developer <kaurparambir638@gmail.com>
2023-09-15 16:13:00 +0530

    fix: fix optimization
    -commit changes of 22-aug-2023 without devops section

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,30 +1,20 @@
-// This file configures the initialization of Sentry on the client.
-// The config you add here will be used whenever a users loads a page in their browser.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+// import * as Sentry from '@sentry/nextjs'
 
-import * as Sentry from "@sentry/nextjs";
+// Sentry.init({
+//   dsn: 'https://188cc9babe6e15b908a2de770363e310@o4504490778951680.ingest.sentry.io/4505724408823808',
 
-Sentry.init({
-  dsn: "https://188cc9babe6e15b908a2de770363e310@o4504490778951680.ingest.sentry.io/4505724408823808",
+//   tracesSampleRate: 1,
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+//   debug: false,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+//   replaysOnErrorSampleRate: 1.0,
 
-  replaysOnErrorSampleRate: 1.0,
+//   replaysSessionSampleRate: 0.1,
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
-
-  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
-  integrations: [
-    new Sentry.Replay({
-      // Additional Replay configuration goes in here, for example:
-      maskAllText: true,
-      blockAllMedia: true,
-    }),
-  ],
-});
+//   integrations: [
+//     new Sentry.Replay({
+//       maskAllText: true,
+//       blockAllMedia: true,
+//     }),
+//   ],
+// })
