dab760463a166be1c75e0b1da2e34067e5075f94
unknown <optium-devloper>
2023-10-20 17:52:05 +0530

    refac: update case-study page design

--- components/case-study/Challenges.jsx
+++ components/case-study/Challenges.jsx
@@ -3,10 +3,17 @@ import style from '../../styles/case-page.module.css'
 import Image from 'next/image'
 const Challenges = ({ Casedata }) => {
   const isVideoChatPro = Casedata.title === 'Mobile App For Divers'
+  const challenges = Casedata.Challenges
+  const totalChallenges = challenges.length
+  const challengesPerColumn = Math.ceil(totalChallenges / 2)
+
+  // Split the challenges array into two columns
+  const leftChallenges = challenges.slice(challengesPerColumn)
+  const rightChallenges = challenges.slice(0, challengesPerColumn)
   return (
     <>
       <div className={style.challenge_main}>
-        <div className={style.challengeimg}>
+        {/* <div className={style.challengeimg}>
           {isVideoChatPro ? (
             <div className={style.challenge_img}>
               <div>
@@ -36,17 +43,54 @@ const Challenges = ({ Casedata }) => {
               </div>
             </>
           )}
-        </div>
-        <div>
+        </div> */}
+        <div className="">
           <div className={listStyle.techservicebodyleft}>
-            <h1 className={style.challenge_header}>Challenges</h1>
-            <ul className={`${listStyle.hirelisticon} ${listStyle.challenges}`}>
-              {Casedata.Challenges.map((challenge, index) => (
-                <li key={index} className={listStyle.marginebottom16px}>
-                  <p className={listStyle.marginleft25px}>{challenge}</p>
-                </li>
-              ))}
-            </ul>
+            <div className={style.challenge_columns}>
+              <div className={style.challenge_column_left}>
+                <h1 className={style.challenge_header}>Challenges</h1>
+                {leftChallenges.map((challenge, index) => (
+                  <div
+                    className={`feature-slide-row d-flex align-items-center py-3 ${style.py_3}`}
+                    key={index}
+                  >
+                    <div className={`logo-container ${style.logo}`}>
+                      <Image
+                        src={challenge.img}
+                        alt=""
+                        width={56}
+                        height={56}
+                      />
+                    </div>
+                    <div className="align-items-center">
+                      <h3>{challenge.heading}</h3>
+                      <p>{challenge.des}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className={style.challenge_column_right}>
+                {rightChallenges.map((challenge, index) => (
+                  <div
+                    className={`feature-slide-row d-flex align-items-center py-3 ${style.py_3}`}
+                    key={index}
+                  >
+                    <div className={`logo-container ${style.logo}`}>
+                      <Image
+                        src={challenge.img}
+                        alt=""
+                        width={56}
+                        height={56}
+                      />
+                    </div>
+                    <div className="align-items-center">
+                      <h3>{challenge.heading}</h3>
+                      <p>{challenge.des}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       </div>
