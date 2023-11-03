e8225e2f4d53302ebca4645b047e6d4ee1b64549
unknown <optium-devloper>
2023-11-03 15:10:26 +0530

    fix: footer position

--- pages/_app.js
+++ pages/_app.js
@@ -41,7 +41,7 @@ function MyApp({ Component, pageProps }) {
   const [isLoading, setIsLoading] = useState(true)
   setTimeout(() => {
     setIsLoading(false)
-  }, 17000)
+  }, 10000)
   // const [auth, setAuth] = useState(false)
   const hideHeader =
     Component === Qrm || Component === Qra || Component === Qrpm
