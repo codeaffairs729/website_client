b0ecc875a4ebcc729017ea4a0397d2d4d4a0fcc5
unknown <optium-devloper>
2023-11-03 16:20:28 +0530

    fix : footer section

--- pages/_app.js
+++ pages/_app.js
@@ -21,8 +21,10 @@ import 'react-toastify/dist/ReactToastify.css'
 const Header = dynamic(() => import('./includes/header'), {
   ssr: false,
 })
-import Footer from '../components/Footer'
 
+ const Footer = dynamic(()=> import('../components/Footer'),{
+  ssr:false
+ })
 import Qrm from '../pages/qrm'
 import Qra from '../pages/qra'
 import Qrpm from '../pages/qrpm'
